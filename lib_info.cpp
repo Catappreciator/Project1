0ddf0f76ae61a00f18bfa3069e2fdaf4859865e6
Kylie Olivia Norosky <knorosky@hydra3.eecs.utk.edu>
2025-01-27 11:28:02 -0500

    initialized lib_info

---
+++ lib_info.cpp
@@ -0,0 +1,22 @@
+struct Song { 
+    string title;
+    int time;  // could also be a string
+};
+
+struct Album {
+    map <int, Song > songs;
+    string name;
+    int time;
+    int nsongs;  // optional variable but makes it easier
+};
+
+struct Artist {
+    map <string, Album > albums;
+    string name;
+    int time;
+    int nsongs;
+};
+
+int main() {
+	return 0;
+}
