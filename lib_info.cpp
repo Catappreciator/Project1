8e43e32e6e9f0e9063f4171e6096086d0cbfb8c8
Ada Jane Whitefield <awhitef3@hydra22.eecs.utk.edu>
2025-01-27 15:53:13 -0500

    Additional map

--- lib_info.cpp
+++ lib_info.cpp
@@ -4,6 +4,7 @@
 
 //I hate Git rn
 #include <iostream>
+#include <map>
 #include <sstream>
 using namespace std;
 
@@ -56,50 +57,53 @@ string convertToString(int seconds) {
     }
     return line;
 }
-
+map<string, Artist> artists;
 int main() {
 	//declare read in variables
 	string title;
 	string artist;
-	int duration;
+	string duration;
 	string album;
 	string genre;
 	int track;
 	
-	map<string, Album>::iterator ait; //iterates through artist
+	map<string, Artist>::iterator sit; //iterates through artist
+	map<string, Album>::iterator ait; //iterates through album
 
 	while(cin >> title >> artist >> duration >> album >> genre >> track) {
-		
+		int tDuration = convertToSec(duration);	//Immediately we can take in the string and convert to int
 		//find artist, if not there insert with album
-		ait = Artist->albums.find(artist); //iterator points where artist is
-		if(ait == Artist->albums.end()) { //if not found
-
+		sit = artists.find(artist); //iterator points where artist is
+		if(sit == artists.end()) { //if not found
+    
 			//create new artist
 			Artist *theArtist = new Artist(); //create new artist
 			//update Artist attributes
 			theArtist->name = artist; //initialized with read in artist variable
-			theArtist->time = duration; //initialized with read in time
+			theArtist->time = tDuration; //initialized with read in time
 			theArtist->nsongs = track; //initialized with read in track
 
 			//create new album
 			Album *theAlbum = new Album(); //create album
 			//update Album attributes
 			theAlbum->name = album; //initialized with read in album name
-			theAlbum->time = duration; //initialized with read in song time
+			theAlbum->time = tDuration; //initialized with read in song time
 			theAlbum->nsongs = 1; //initialized with the 1 song inserted
 
 			//create new song
 			Song *theSong = new Song();
 			theSong->title = title; //initilized with read in song
-			theSong->time = duration; //initialized with read in song length
+			theSong->time = tDuration; //initialized with read in song length
 			
+			//This is the point where neovim starts to fuss
+	    
 
 			//connect to Artist map with Album struct
 			theArtist->albums.insert(make_pair(artist, theAlbum));
 			//connect to Album map with Song struct
 			theAlbum->songs.insert(track, title);
 			theSong->title = title;
-			theSong->time = duration;
+			theSong->time = tDuration;
 
 
 			//insert artist name and album in TheArtist albums map
@@ -109,6 +113,8 @@ int main() {
 		}
 		else{
 		    
+		//ait = Artist->albums.find(artist); //iterator points where artist is --> need to check for album name here
+		//if(ait == Artist->albums.end()) { //if not found
 
 		}
 	}
