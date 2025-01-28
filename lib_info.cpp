3c928fd106e67120294638a94218783ef0056da9
Ada Jane Whitefield <awhitef3@hydra22.eecs.utk.edu>
2025-01-28 06:28:03 -0500

    Basic output algorithm

--- lib_info.cpp
+++ lib_info.cpp
@@ -58,6 +58,17 @@ string convertToString(int seconds) {
     }
     return line;
 }
+//Removes underscore in string variables
+string stripUnderscore(string name){
+    for(int i = 0; i < name.size(); i++){
+	if(name[i] == '_'){
+	    name[i] = ' ';
+	}
+    }
+    return name;
+
+}
+
 int main() {
 	//declare read in variables
 	string title;
@@ -70,6 +81,9 @@ int main() {
 	map<string, Artist> artists;
 	map<string, Artist>::iterator sit; //iterates through artist
 	map<string, Album>::iterator ait; //iterates through album
+    
+
+	//TODO: Add up time for both artists and albums separately
 
 	while(cin >> title >> artist >> duration >> album >> genre >> track) {
 		int tDuration = convertToSec(duration);	//Immediately we can take in the string and convert to int
@@ -82,6 +96,8 @@ int main() {
 			//Artist *theArtist = new Artist(); //create new artist
 			//update Artist attributes
 			Artist theArtist;
+			artists.insert(make_pair(artist, theArtist));
+			//artist = stripUnderscore(artist);
 			artists.insert(make_pair(artist, theArtist));
 			theArtist.name = artist; //initialized with read in artist variable
 			theArtist.time = tDuration; //initialized with read in time
@@ -130,17 +146,50 @@ int main() {
 
 			}
 			else { //album exists
-				cout << "Old Album: " << album << endl; //TODO formatting
+				cout << "Old Album: " << album << endl; 
+				//Increment number of songs if album already exists
+				ait->second.nsongs++;
 			}
 			//insert songs into album song list and update album total song time
 			Song theSong;
 			theSong.title = title; //initilized with read in song
 			theSong.time = tDuration; //initialized with read in song length
 			ait->second.songs.insert(make_pair(track, theSong));
+
+
+	//Increment nsongs if artist already exists		
+			sit->second.nsongs++;
+			
 		
 			//print songs in correct format: track & time
 		}
 	}
+
+    //grayed out stuff is where I started on basic skeleton of output algorithm, don't want to run anything with it yet because it'll make things more confusing.
+
+/*	map<string, Artist>::iterator mit;
+	map<string, Album>::iterator oit;
+	map<int, Song>::iterator pit;
+//Loop through artists, maps should make it in lexographic order
+	for(mit = artists.begin(); mit != artists.end(); mit++){
+	//Convert time back into string format
+	    string artistTime = convertToString(mit->second.time);
+	    cout << mit->first << ':' << mit->second.nsongs << ', ' << artistTime << endl;
+	    //Output artist's albums
+	    for(oit = mit->second.albums.begin(); oit != mit->second.albums.end(); oit++){
+		string albumTime = convertToString(oit->second.time);
+		cout << oit->first << ': ' << oit->second.nsongs << ", " << albumTime << endl;
+		for(pit = oit->second.songs.begin(); pit != oit->second.songs.end(); pit++){
+		    string songTime = convertToString(pit->second.time);
+		    cout << pit->first << ". " << pit->second.title << ": " << songTime << endl;
+		}
+
+	    }
+	//General pattern to follow for output:
+	
+	}*/
+	
+	
 	
 	return 0;
 }
